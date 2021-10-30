30a5debc504b5113c5333770d6eaf19aa358f620
Waca <guaca33@gmail.com>
2021-10-29 21:55:09 -0300

    CH14

---
+++ js/comparando.js
@@ -0,0 +1,12 @@
+// COMPARANDO NUMEROS
+
+let x = Number(prompt('Ingrese un numero'));
+let y = Number(prompt('Ingrese otro numero'));
+
+if (x > y){
+    alert('El 1er numero es mayor')
+}else if (x == y){
+    alert ('Los numeros son iguales')
+}else{
+    alert('El 1er numero es menor')
+}
